c244cb6292f3effbf32cebe328ffaa5bfa79885c
Andrii Lehendzevych <andrii.lehendzevych@gmail.com>
2025-09-08 15:33:02 +0200

    Created header block

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+const burgerWrap = document.querySelector('.header__mobile-burger');
+const burgerBtn = burgerWrap.querySelector('button');
+const burgerSvg = document.querySelector('.header__mobile-burger-svg');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burgerBtn.addEventListener('click', () => {
+  burgerSvg.classList.toggle('header__mobile-burger-svg--active');
+  mobileMenu.classList.toggle('mobile-menu--active');
+});
